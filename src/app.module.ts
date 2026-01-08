a4b1a45535beb52081b3bdb96808ae740ea5cb53
Nanret123 <nanretgungshik@gmail.com>
2026-01-08 15:48:24 +0100

    add worker module

--- src/app.module.ts
+++ src/app.module.ts
@@ -45,38 +45,10 @@ import { SubscriptionGuard } from './common/guards/subscription.guard';
 import { MetaWebhooksModule } from './meta-webhooks/meta-webhooks.module';
 import { PostModule } from './post/post.module';
 import { PostMediaModule } from './post-media/post-media.module';
+import { WorkerModule } from './worker/worker.module';
 
 @Module({
   imports: [
-    BullModule.forRootAsync({
-      useFactory: () => {
-        // 1. If running on Render (Cloud)
-        if (process.env.REDIS_URL) {
-          const url = new URL(process.env.REDIS_URL);
-          return {
-            connection: {
-              host: url.hostname,
-              port: Number(url.port),
-              username: url.username,
-              password: url.password,
-              tls: {
-                rejectUnauthorized: false, // Essential for Upstash/Render
-              },
-            },
-            skipConfigValidation: true,
-          };
-        }
-
-        //2. If running Locally
-        return {
-          connection: {
-            host: process.env.REDIS_HOST || 'localhost',
-            port: Number(process.env.REDIS_PORT || 6379),
-            password: process.env.REDIS_PASSWORD,
-          },
-        };
-      },
-    }),
 
     ConfigModule.forRoot({
       isGlobal: true,
@@ -152,6 +124,8 @@ import { PostMediaModule } from './post-media/post-media.module';
     PostModule,
 
     PostMediaModule,
+
+    WorkerModule,
   ],
   controllers: [AppController],
   providers: [
