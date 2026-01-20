659c9d95ececa77e8414f9db451b93ff5c3450d3
Nanret123 <nanretgungshik@gmail.com>
2026-01-20 16:31:22 +0100

    fix

--- src/worker/worker.module.ts
+++ src/worker/worker.module.ts
@@ -11,41 +11,41 @@ import { EncryptionService } from '@/common/utility/encryption.service';
 
 @Module({
   imports: [
-    // BullModule.forRootAsync({
-    //       useFactory: () => {
-    //         // 1. If running on Render (Cloud)
-    //         if (process.env.REDIS_URL) {
-    //           const url = new URL(process.env.REDIS_URL);
-    //           return {
-    //             connection: {
-    //               host: url.hostname,
-    //               port: Number(url.port),
-    //               username: url.username,
-    //               password: url.password,
-    //               tls: {
-    //                 rejectUnauthorized: false, // Essential for Upstash/Render
-    //               },
-    //             },
-    //             skipConfigValidation: true,
-    //           };
-    //         }
+    BullModule.forRootAsync({
+          useFactory: () => {
+            // 1. If running on Render (Cloud)
+            if (process.env.REDIS_URL) {
+              const url = new URL(process.env.REDIS_URL);
+              return {
+                connection: {
+                  host: url.hostname,
+                  port: Number(url.port),
+                  username: url.username,
+                  password: url.password,
+                  tls: {
+                    rejectUnauthorized: false, // Essential for Upstash/Render
+                  },
+                },
+                skipConfigValidation: true,
+              };
+            }
     
-    //         //2. If running Locally
-    //         return {
-    //           connection: {
-    //             host: process.env.REDIS_HOST || 'localhost',
-    //             port: Number(process.env.REDIS_PORT || 6379),
-    //             password: process.env.REDIS_PASSWORD,
-    //           },
-    //         };
-    //       },
-    //     }),
-    //     BullModule.registerQueue({
-    //   name: 'media-ingest', 
-    // }),
-    // BullModule.registerQueue({
-    //   name: 'publishing-queue', 
-    // }),
+            //2. If running Locally
+            return {
+              connection: {
+                host: process.env.REDIS_HOST || 'localhost',
+                port: Number(process.env.REDIS_PORT || 6379),
+                password: process.env.REDIS_PASSWORD,
+              },
+            };
+          },
+        }),
+        BullModule.registerQueue({
+      name: 'media-ingest', 
+    }),
+    BullModule.registerQueue({
+      name: 'publishing-queue', 
+    }),
     PostMediaModule,
     SocialModule
   ],
